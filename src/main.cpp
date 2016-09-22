d313728961895c528d0a0d64371798441cfdbe2c
lead999x <lead999x@gmail.com>
2016-09-22 01:56:15 -0400

    Added Frontend to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,24 @@
-int main(int argc, char * argv[]) {
+#include <iostream>
+#include <string>
+
+#include "cas_frontend.hpp"
+
+
+int main(int argc, char * argv[])
+{
+	std::string command(""), expression("");
+	bool quit(false);
+
+	std::cout << "C++ CAS\nversion 0.0\n";
+	while(true)//primary I/O loop
+	{
+		std::cout << "> ";
+		CAS::separate_input(CAS::get_input(std::cin), &command, &expression);
+		CAS::handle_inputs(CAS::make_command(command), expression);-
+
+		//if the handle_inputs command runs into the quit command it will set bool quit to true and execution will end
+		quit ? break : continue;
+	}
+
 	return 0;
 }
