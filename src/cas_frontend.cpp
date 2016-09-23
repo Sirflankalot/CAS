2de3885f21be4b9ca9360279d0a35ba0f025f2e4
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2016-09-22 22:47:37 -0400

    Auto format code

--- src/cas_frontend.cpp
+++ src/cas_frontend.cpp
@@ -1,59 +1,61 @@
 #include "cas_frontend.hpp"
 
-#include <iostream>
 #include <cstdlib>
+#include <iostream>
 
 std::string CAS::get_input(std::istream& input_stream)
 {
-    std::string input_line;
-    std::getline(input_stream, input_line,'\n');
-    return input_line;
+	std::string input_line;
+	std::getline(input_stream, input_line, '\n');
+	return input_line;
 }
 
 void CAS::separate_input(const std::string& input, std::string& command, std::string& expression)
 {
-    size_t i = 0;//current character index in the input string
+	size_t i = 0; // current character index in the input string
 
-    //sets the value of the command string variable to the first word in the input string
-    for(i = 0; i < input.size(); ++i)
-    {
-        if(input[i] != ' ')
-        {
-            command.push_back(input[i]);
-        }
+	// sets the value of the command string variable to the first word in the input string
+	for (i = 0; i < input.size(); ++i)
+	{
+		if (input[i] != ' ')
+		{
+			command.push_back(input[i]);
+		}
 
-        //stops adding letters to command after reaching the first space in input
-        else
-        {
-            i = i + 1;
-            break;
-        }
-    }
+		// stops adding letters to command after reaching the first space in input
+		else
+		{
+			i = i + 1;
+			break;
+		}
+	}
 
-    //skips the first space and adds the remaining characters in input to the string expression which holds the mathematical expression
-    //to be solved
-    for(; i < input.size(); i++)
-    {
-        expression.push_back(input[i]);
-    }
+	// skips the first space and adds the remaining characters in input to the string expression
+	// which holds the mathematical expression
+	// to be solved
+	for (; i < input.size(); i++)
+	{
+		expression.push_back(input[i]);
+	}
 }
 
 CAS::command CAS::make_command(const std::string& raw_command)
 {
-    //use if/else if to convert the std::string raw_command to its CAS::commands counterpart for use with CAS::handle_inputs
-    /* prototype:
+	// use if/else if to convert the std::string raw_command to its CAS::commands counterpart for
+	// use with CAS::handle_inputs
+	/* prototype:
 
-    if(raw_command == "example") return CAS::commands::example;
-    else if(raw_command == "other") return CAS::commands::other;
-    else return CAS::commands::invalid_command;
-    */
+	if(raw_command == "example") return CAS::commands::example;
+	else if(raw_command == "other") return CAS::commands::other;
+	else return CAS::commands::invalid_command;
+	*/
 }
 
 void CAS::handle_inputs(const CAS::command& command, const std::string& argument)
 {
-    switch(command)
-    {
-        default:
-            break;
-    }
+	switch (command)
+	{
+		default:
+			break;
+	}
 }
--- src/cas_frontend.hpp
+++ src/cas_frontend.hpp
@@ -5,27 +5,33 @@
 
 namespace CAS
 {
-    //get_input: std::istream -> std::string
-    //takes an std::iostream and gets a line from it which is then returned with no trailing space or newline
-    //std::cin is not explicitly used so that this function can also read from std::fstream and std::stringstream objects
-    std::string get_input(std::istream&);
+	// get_input: std::istream -> std::string
+	// takes an std::iostream and gets a line from it which is then returned with no trailing space
+	// or newline
+	// std::cin is not explicitly used so that this function can also read from std::fstream and
+	// std::stringstream objects
+	std::string get_input(std::istream&);
 
-    //separate_input: std::string &std::string &std::string -> void
-    //takes in a string and two string references
-    //separates the first string into a command and math expression
-    void separate_input(const std::string&, std::string&, std::string&);
+	// separate_input: std::string &std::string &std::string -> void
+	// takes in a string and two string references
+	// separates the first string into a command and math expression
+	void separate_input(const std::string&, std::string&, std::string&);
 
-    /*Provides an enum with all legal commands in the C++ CAS language and a function to convert the std::string command into its
-    commands equivalent. This allows for switch statements to easily be used in the function handle_inputs.*/
-    enum command
-    {
-        //legal commands in the C++ CAS language will be listed here and are used by the handle_inputs function
-    };
-    CAS::command make_command(const std::string&);
+	/*Provides an enum with all legal commands in the C++ CAS language and a function to convert the
+	std::string command into its
+	commands equivalent. This allows for switch statements to easily be used in the function
+	handle_inputs.*/
+	enum command
+	{
+		// legal commands in the C++ CAS language will be listed here and are used by the
+		// handle_inputs function
+	};
+	CAS::command make_command(const std::string&);
 
-    //handle_input: const std::string const std::string -> void
-    //take the appropriate action based on the command passed in and using the given math expression
-    void handle_inputs(const CAS::command&, const std::string&);
+	// handle_input: const std::string const std::string -> void
+	// take the appropriate action based on the command passed in and using the given math
+	// expression
+	void handle_inputs(const CAS::command&, const std::string&);
 }
 
-#endif //CAS_FRONTEND
+#endif // CAS_FRONTEND
